8759216b8b6660e4a8e27092b4e509de5e92d70e
Val <kantotemus@gmail.com>
2026-03-03 12:27:00 +0600

    Fix routing: stable HashRouter with slug param

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, useParams } from "react-router-dom";
 import PublicSalonPage from "./public/PublicSalonPage";
 import BookingPage from "./room/BookingPage";
 import SalonBookingsPage from "./room/SalonBookingsPage";
@@ -11,17 +11,25 @@ function SalonRoom() {
   return <div style={{ padding: 20 }}>Salon Room</div>;
 }
 
-export default function App({ slug }) {
+function SalonWrapper() {
+  const { slug } = useParams();
+
+  return (
+    <Routes>
+      <Route index element={<PublicSalonPage slug={slug} />} />
+      <Route path="booking" element={<BookingPage slug={slug} />} />
+      <Route path="bookings" element={<SalonBookingsPage slug={slug} />} />
+      <Route path="room" element={<MasterRoom />} />
+      <Route path="salon" element={<SalonRoom />} />
+    </Routes>
+  );
+}
+
+export default function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route index element={<PublicSalonPage slug={slug} />} />
-
-        <Route path="booking" element={<BookingPage slug={slug} />} />
-        <Route path="bookings" element={<SalonBookingsPage slug={slug} />} />
-
-        <Route path="room" element={<MasterRoom />} />
-        <Route path="salon" element={<SalonRoom />} />
+        <Route path="/salon/:slug/*" element={<SalonWrapper />} />
       </Routes>
     </HashRouter>
   );
