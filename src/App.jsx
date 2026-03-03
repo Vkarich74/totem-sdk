2340e1150f91e916da42e57ba211e7078894eb64
Val <kantotemus@gmail.com>
2026-03-04 00:21:40 +0600

    Fix duplicate App.jsx declarations

--- src/App.jsx
+++ src/App.jsx
@@ -28,49 +28,10 @@ export default function App() {
         <Route path="booking" element={<BookingPage slug={slug} />} />
         <Route path="bookings" element={<SalonBookingsPage slug={slug} />} />
 
-        {/* OWNER DASHBOARD */}
+        {/* OWNER */}
         <Route path="owner" element={<OwnerDashboard slug={slug} />} />
 
-        {/* LEGACY / PLACEHOLDER */}
-        <Route path="room" element={<MasterRoom />} />
-        <Route path="salon" element={<SalonRoom />} />
-      </Routes>
-    </HashRouter>
-  );
-}import { HashRouter, Routes, Route } from "react-router-dom";
-import PublicSalonPage from "./public/PublicSalonPage";
-import BookingPage from "./room/BookingPage";
-import SalonBookingsPage from "./room/SalonBookingsPage";
-import OwnerDashboard from "./owner/OwnerDashboard";
-
-function getSlugFromPath() {
-  const parts = window.location.pathname.split("/");
-  return parts[2] || null; // /salon/:slug
-}
-
-function MasterRoom() {
-  return <div style={{ padding: 20 }}>Master Room</div>;
-}
-
-function SalonRoom() {
-  return <div style={{ padding: 20 }}>Salon Room</div>;
-}
-
-export default function App() {
-  const slug = getSlugFromPath();
-
-  return (
-    <HashRouter>
-      <Routes>
-        {/* PUBLIC */}
-        <Route index element={<PublicSalonPage slug={slug} />} />
-        <Route path="booking" element={<BookingPage slug={slug} />} />
-        <Route path="bookings" element={<SalonBookingsPage slug={slug} />} />
-
-        {/* OWNER DASHBOARD */}
-        <Route path="owner" element={<OwnerDashboard slug={slug} />} />
-
-        {/* LEGACY / PLACEHOLDER */}
+        {/* PLACEHOLDER */}
         <Route path="room" element={<MasterRoom />} />
         <Route path="salon" element={<SalonRoom />} />
       </Routes>
