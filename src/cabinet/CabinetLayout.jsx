f5bfcf9b10ebe1fe004e789952537826c7df9893
Val <kantotemus@gmail.com>
2026-03-09 19:59:01 +0600

    Cabinet Engine: scroll isolation (content + CMS)

--- src/cabinet/CabinetLayout.jsx
+++ src/cabinet/CabinetLayout.jsx
@@ -13,7 +13,8 @@ export default function CabinetLayout({
       display: "flex",
       flexDirection: "column",
       height: "100vh",
-      width: "100%"
+      width: "100%",
+      overflow: "hidden"
     }}>
 
       {header}
@@ -21,21 +22,24 @@ export default function CabinetLayout({
       <div style={{
         display: "flex",
         flex: 1,
-        minHeight: 0
+        minHeight: 0,
+        overflow: "hidden"
       }}>
 
         <div style={{
           width: "70%",
           borderRight: "1px solid #eee",
           display: "flex",
-          minHeight: 0
+          minHeight: 0,
+          overflow: "hidden"
         }}>
 
           {sidebar}
 
           <div style={{
             flex: 1,
-            overflow: "auto",
+            overflowY: "auto",
+            overflowX: "hidden",
             padding: "20px",
             minHeight: 0,
             minWidth: 0
@@ -47,7 +51,16 @@ export default function CabinetLayout({
 
         </div>
 
-        {odoo}
+        <div style={{
+          width: "30%",
+          minHeight: 0,
+          overflowY: "auto",
+          overflowX: "hidden"
+        }}>
+
+          {odoo}
+
+        </div>
 
       </div>
 
