53dec3ab512163b69f0f26d525b0c133d3970ac5
Val <kantotemus@gmail.com>
2026-04-24 14:25:38 +0600

    phase9: add admin logout navigation

--- src/admin/AdminNavigation.jsx
+++ src/admin/AdminNavigation.jsx
@@ -1,4 +1,9 @@
 export default function AdminNavigation(){
+  function handleLogout(){
+    localStorage.removeItem("TOTEM_AUTH_TOKEN");
+    window.location.assign("#/admin/login");
+  }
+
   return (
     <div style={{ marginBottom: 16 }}>
       <a href="#/admin/messages">Сообщения</a>
@@ -8,6 +13,10 @@ export default function AdminNavigation(){
       <a href="#/admin/cases">Кейсы</a>
       <span> | </span>
       <a href="#/admin/login">Логин</a>
+      <span> | </span>
+      <button type="button" onClick={handleLogout}>
+        Выход
+      </button>
     </div>
   )
 }
